c52e2c08899f1712efb78d39a3f07bd453929d10
KABEYI <eugenekabeyi144@gmail.com>
2025-11-15 21:23:25 +0300

    Modified query_sample

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -6,29 +6,33 @@ from relationship_app.models import Author, Book, Library, Librarian
 # -----------------------------------------
 
 author_name = "George Orwell"
-author = Author.objects.filter(name=author_name).first()
 
 print("\n Books by", author_name + ":")
-if author:
-    books = author.books.all()
-    for book in books:
-        print(" -", book.title)
-else:
+ 
+try:
+    author = Author.objects.get(name=author_name)
+    if author:
+        books = author.books.all()
+        for book in books:
+            print(" -", book.title)
+except Author.DoesNotExist:
     print("Author not found!")
 
+
 # -----------------------------------------
-# 2. List all books in a library
+# 2. List all books in a library (using .get)
 # -----------------------------------------
 
 library_name = "Central Library"
-library = Library.objects.filter(name=library_name).first()
 
 print("\n Books in", library_name + ":")
-if library:
+try:
+    library = Library.objects.get(name=library_name)
     for book in library.books.all():
         print(" -", book.title)
-else:
+except Library.DoesNotExist:
     print("Library not found!")
+
 
 # -----------------------------------------
 # 3. Retrieve the librarian for a library
