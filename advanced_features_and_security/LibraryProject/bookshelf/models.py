4f87fa668bf69655fbffb11c53a19a0edd8fa82f
KABEYI <eugenekabeyi144@gmail.com>
2025-11-16 07:30:56 +0300

    Modifiesbookshelf model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# ----------------------------------------
+# CUSTOM USER MODEL
+# ----------------------------------------
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+# ----------------------------------------
+# EXISTING BOOK MODEL
+# ----------------------------------------
 class Book(models.Model):
-    title =models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
