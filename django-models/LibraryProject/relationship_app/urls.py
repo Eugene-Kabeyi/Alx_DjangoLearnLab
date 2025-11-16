b7772eed5829f26f8ccb234c3f5f6b4e7e9605c3
KABEYI <eugenekabeyi144@gmail.com>
2025-11-16 06:34:12 +0300

    Added Authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView  # imports both FBV + CBV
+from .views import (
+    list_books,
+    LibraryDetailView,
+    register_view,
+    login_view,
+    logout_view,
+)
+ # imports both FBV + CBV
 
 urlpatterns = [
     # Function-based view to list all books
@@ -7,4 +14,8 @@ urlpatterns = [
     
     # Class-based view to show library details
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("register/", register_view, name="register"),
 ]
