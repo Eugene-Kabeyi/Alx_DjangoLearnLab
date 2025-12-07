180e95266e97858d218b5da56fd2021707592fee
KABEYI <eugenekabeyi144@gmail.com>
2025-12-07 20:52:03 +0300

    Changes model : Comments

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -26,7 +26,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)  # updates automatically on save
     
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
