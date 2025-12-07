d485d94e67a94f047292e7bda2420a69e04c0d4c
KABEYI <eugenekabeyi144@gmail.com>
2025-12-07 08:27:32 +0300

    Workedon models

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User   # ← REQUIRED for the Post author field
 
 # Create your models here.
 class Author(models.Model):
@@ -11,7 +12,7 @@ class Post (models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title
