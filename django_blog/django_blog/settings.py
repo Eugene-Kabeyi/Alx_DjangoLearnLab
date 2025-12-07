bd163c0824f75494f5419dcb62247ee4f3e4f38e
KABEYI <eugenekabeyi144@gmail.com>
2025-12-07 08:32:08 +0300

    Workedon db

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',   # MariaDB uses MySQL backend
+        'NAME': 'django_blog_db',               # Your database name
+        'USER': 'root',                         # Your MariaDB username
+        'PASSWORD': 'kabeyi123',            # Your MariaDB password
+        'HOST': '127.0.0.1',                    # Local host
+        'PORT': '3306',                         # Default MariaDB/MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
