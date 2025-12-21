4a510481e45e9374e33b31f2a412a62ae412e03e
KABEYI <eugenekabeyi144@gmail.com>
2025-12-21 23:36:18 +0300

    Changes made on the views and urls

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Post, Comment
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import PostSerializer, CommentSerializer
 
 class IsOwner(permissions.BasePermission):
@@ -25,3 +27,18 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
+class FeedView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        # Must use following.all() exactly
+        following_users = request.user.following.all()
+
+        # Must use this exact filter + order_by pattern
+        posts = Post.objects.filter(
+            author__in=following_users
+        ).order_by('-created_at')
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
